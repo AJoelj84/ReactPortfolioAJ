d96d49ea307f24cf5948f8d350ff906936c51725
AJoelj84 <ajoelj84@gmail.com>
2023-07-09 22:25:22 -0400

    styling of Project/Portfolio Cards

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import Onlycheese from '../assets/images/Onlycheese.jpg';
 import GroupProject1 from '../assets/images/groupproject.jpg';
-import HTML from '../assets/images/html.jpg'
-import PLACEHOLDER from '../assets/images/keyboard.jpeg';
+import TechQik from '../assets/images/techqik.jpg';
+import WorkDay from '../assets/images/workday.jpg';
+import BookSearch from '../assets/images/booksearch.jpg';
+import WeatherDash from '../assets/images/weatherdash.jpg';
+
 
 
 function Projects() {
@@ -11,38 +14,66 @@ function Projects() {
     <section id="Projects">
       <h1>Portfolio</h1>
 
-      <p className="linktext">Group Project 2</p>
-        <a href="https://only-cheese.herokuapp.com/" alt="Group Project 2">
-          <img src= {Onlycheese} alt="Group Project 2" width="900" height="500" />
-        </a>
-    
+  <div class="card-container row row-cols-1 row-cols-sm-2 g-3">
+    <div class="col">
+      <a href="https://only-cheese.herokuapp.com/" alt="Group Project 2" style={{ textDecoration: 'none' }}>
+        <div class="card">
+          <img src={Onlycheese} class="card-img-top" alt="card-grid-image"/>
+          <div class="card-body">
+          <h5 class="card-title">Group Project #2</h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+    </div>
+      </a>
+  </div>
 
-        <p className="linktext">Group Project 1</p>
-        <a href="https://brandon5667.github.io/peak-fitness/" alt="First Group Project Link">
-          <img src= {GroupProject1} alt="Group Project 1" width="900" height="500" />
-        </a>
-        
-      
 
-        <p className="linktext">HTML/CSS</p>
-        <a href="https://github.com/AJoelj84/Horiseon-Optimization" alt="HTML Image">
-          <img src= {HTML} alt="HTML Image" width="800" height="500" />
-        </a>
-        
-      
+  <div class="col">
+  <a href="https://brandon5667.github.io/peak-fitness/" alt="First Group Project Link" style={{ textDecoration: 'none' }}>
+    <div class="card">
+      <img src={GroupProject1}  class="card-img-top" alt="card-grid-image"/>
+      <div class="card-body">
+        <h5 class="card-title">Group Project #1</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </a>
+  </div>
 
-        <p className="linktext">Place Holder 1</p>
-        <a href="./pages/placeholder.html" alt="Placeholder Page">
-          <img src= {PLACEHOLDER} alt="Keyboard Image" width="900" height="500" />
-        </a>
-        
-    
 
-        <p className="linktext">Place Holder 2</p>
-        <a href="./pages/placeholder.html" alt="Placeholder Page">
-          <img src= {PLACEHOLDER} alt="Keyboard Image" width="900" height="500" />
-        </a>
-        
+  <div class="col">
+  <a href="https://techqik-ccaa5c6abf81.herokuapp.com/" alt="Tech blog Project" style={{ textDecoration: 'none' }}>
+    <div class="card">
+      <img src={TechQik} class="card-img-top" alt="card-grid-image"/>
+      <div class="card-body">
+        <h5 class="card-title">TechQik TechBlog Project</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+    </a>
+  </div>
+
+  
+  <div class="col">
+  <a href="https://ajoelj84.github.io/WeatherDashboard/" alt="Weather Dashboard Project" style={{ textDecoration: 'none' }}>
+    <div class="card">
+      <img src={WeatherDash} class="card-img-top" alt="card-grid-image"/>
+      <div class="card-body">
+        <h5 class="card-title">Weather Dashboard Project</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </a>  
+  </div>
+</div>
+
+
+
+
+
+
+
+
     
     </section>
     </div>
