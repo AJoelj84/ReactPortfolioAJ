10f5b8c778be1aed022bc19eb2f0297918119fb8
AJoelj84 <ajoelj84@gmail.com>
2023-07-09 23:08:26 -0400

    added email form to Contact.js

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,18 +1,101 @@
-import React from 'react';
+import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
 
 function Contact() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const templateParams = {
+      from_name: name,
+      from_email: email,
+      message: message,
+    };
+
+    emailjs
+      .send('service_r1jpyff', 'template_i7dp36q', templateParams, 'Nzwz_PRyVrr5NrlR1')
+      .then(
+        (response) => {
+          console.log('Email sent successfully');
+          setSuccessMessage('Email sent successfully');
+          setErrorMessage('');
+        },
+        (error) => {
+          console.error('Failed to send email:', error);
+          setSuccessMessage('');
+          setErrorMessage('Failed to send email');
+        }
+      );
+  };
+
   return (
-    <div>
+    <div className="contact-container">
+      <h1 className='bodyheaders' style={{ fontFamily: 'Josefin Sans, sans-serif' }}>
+        Contact Me
+      </h1>
 
-      <h1 class = 'bodyheaders' style={{fontFamily: 'Josefin Sans, sans-serif'}}>Contact Me</h1> 
+      <section>
+        <p style={{ fontFamily: 'Josefin Sans, sans-serif' }} className='links'>
+          <a className='contact-link' href='mailto:ajoelj84@gmail.com'>
+            Email
+          </a>
+        </p>
+        <p style={{ fontFamily: 'Josefin Sans, sans-serif' }} className='links'>
+          <a className='contact-link' href='https://github.com/AJoelj84'>
+            GitHub
+          </a>
+        </p>
+      </section>
 
-        <section>
-          <p style={{fontFamily: 'Josefin Sans, sans-serif'}} class= 'links'><a className='contact-link' href="mailto:ajoelj84@gmail.com">Email</a></p>  
-          <p style={{fontFamily: 'Josefin Sans, sans-serif'}} class= 'links'><a className='contact-link' href="https://github.com/AJoelj84">GitHub</a></p>
-        </section>
+      <div className="form-container">
+        {successMessage && <p style={{ color: 'green' }}>{successMessage}</p>}
 
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor='name'>Name:</label>
+            <input
+              type='text'
+              id='name'
+              name='name'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+
+          <div className="input-group">
+            <label htmlFor='email'>Email:</label>
+            <input
+              type='email'
+              id='email'
+              name='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="input-group">
+            <label htmlFor='message'>Message:</label>
+            <textarea
+              id='message'
+              name='message'
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            ></textarea>
+          </div>
+
+          <button type='submit'>Send</button>
+        </form>
+
+        {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+      </div>
     </div>
   );
 }
 
 export default Contact;
+
