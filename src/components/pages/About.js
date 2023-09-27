65b560d558d53eec96f30b4f814bb79177669bfb
AJoelj84 <ajoelj84@gmail.com>
2023-09-26 23:37:07 -0400

    making changes to formatting and adding items

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import ProfilePic from '../assets/images/profilepicture.jpg';
+import CredlyPic from '../assets/images/credly.png';
+
 
 function About() {
   return (
     <div>
       <h1 className='bodyheaders'>About Me</h1>
       <section style={{ fontFamily: 'Josefin Sans, sans-serif' }} className='bodytext'>
-        <div className="row">
-          <div className="col-md-6 order-md-2 text-center">
-            <img src={ProfilePic} alt="Adam Johnson Profile Picture" style={{ width: '500px', height: 'auto' }} />
-          </div>
-          <div className="col-md-6 order-md-1">
-            <div style={{ marginLeft: '80px', marginRight: 'auto' }}>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 mb-4 bodytext">
               <p>
-                Hello! My name is Adam Johnson. I live in the beautiful Smokey Mountains of North Carolina 
-                with my wife and two wild little boys. I have professional experience in multiple industries 
+                Hello! My name is Adam Johnson.
+                I live in the beautiful Smokey Mountains of North Carolina 
+                with my wife and two wild little boys.  I have professional experience in multiple industries 
                 the most experience being an Automotive Mechanic 10 years and Locomotive Engineering for the past 8 years.<br></br>
                 <br></br> 
                 I enjoy spending time with my Family, Fishing, Gardening, Woodworking, Blacksmithing, 3D Printing and any kind of craft/trade that involves 
@@ -30,6 +30,14 @@ function About() {
                 Please feel free to review my portfolio and reach out to me if you have any questions or would like to discuss a project. 
                 I look forward to working with you!
               </p>
+              </div>
+            <div className="col-md-6 mb-4 image">
+              <div className="mb-3">
+                <img src={CredlyPic} alt="Credly Certification Badge" style={{ maxWidth: '600px', height: 'auto' }} />
+              </div>
+              <div className="mb-3 imagetop">
+                <img src={ProfilePic} alt="Adam Johnson Profile Picture" style={{ maxWidth: '500px', height: 'auto' }} />
+              </div>
             </div>
           </div>
         </div>
