f24245dd6037abea391b227b9ea1d410facf74ce
AJoelj84 <ajoelj84@gmail.com>
2023-10-02 10:52:19 -0400

    Made Changes to Bottom Link, Top Nav,  for Mobile

--- src/components/pages/Footer.js
+++ src/components/pages/Footer.js
@@ -5,9 +5,9 @@ function Footer() {
   return (
     <footer style={{fontFamily: 'Josefin Sans, sans-serif'}} className="footer">
       <div className="background">
-        <div className="app-wrapper">
+        <div>
           <div className="social-links">
-            <p className = 'footlink'>More Links </p>
+            <p className = 'footlink'>More Links</p>   
             <a href="mailto:ajoelj84@gmail.com">Email</a>
             <a href="https://github.com/AJoelj84">GitHub</a>
             <a href="https://linkedin.com/in/adam-johnson-569184283">LinkedIn</a>
