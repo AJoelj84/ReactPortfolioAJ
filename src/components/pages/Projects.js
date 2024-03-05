889a538f25c4ccef375db94ed4eeeaa49e5b2fe8
AJoelj84 <ajoelj84@gmail.com>
2024-03-05 16:36:29 -0500

    Changes to Projects page

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -6,22 +6,39 @@ import WorkDay from '../assets/images/workday.jpg';
 import Inked from '../assets/images/Inkedin.jpg';
 import WeatherDash from '../assets/images/weatherdash.jpg';
 import NoteTaker from '../assets/images/NoteTaker.jpg';
+import Portfolio from '../assets/images/Portfolio.jpg';
 
 function Projects() {
   return (
     <div className="bodyheaders" style={{ fontFamily: 'Josefin Sans, sans-serif' }}>
       <section id="Projects">
         <h1>Portfolio</h1>
+        
 
         <div className="card-container row row-cols-1 row-cols-sm-2 g-3">
+          
+        <div className="col">
+            <div className="card cardborder">
+              <a href="https://github.com/AJoelj84/ReactPortfolioAJ" alt="Portfolio Link" style={{ textDecoration: 'none' }}>
+                <img src={Portfolio} className="card-img-top" alt="card-grid-image" />
+              </a>
+                <div className="card-body">
+                  <h5 className="card-title"><br></br>Portfolio Code<br></br><br></br></h5>
+                  <p className="card-text">Here you can view the code and screenshots over time of the development of My Personal Portfolio.  
+                  <br></br><br></br>
+                    Code Repository  <a href="https://github.com/AJoelj84/ReactPortfolioAJ">Here</a>.
+                  </p>
+                </div>
+              </div> 
+          </div>
+
           <div className="col">
-            
               <div className="card cardborder">
                 <a href="https://only-cheese.herokuapp.com/" alt="Group Project 2" style={{ textDecoration: 'none' }} className="d-block">
                   <img src={Onlycheese} className="card-img-top" alt="card-grid-image" />
                 </a>
                   <div className="card-body">
-                    <h5 className="card-title"><a href="https://only-cheese.herokuapp.com/"></a>Only Cheese Group Project #2</h5>
+                    <h5 className="card-title"><a href="https://only-cheese.herokuapp.com/"></a>Only Cheese UNC Group Project #2</h5>
                     <p className="card-text">
                       In this Project, I designed our wireframe and laid the early framework of the site.<br></br>
                       As well I helped set up the Users Profile Card Pages, and the final styling of the site.<br></br>
@@ -39,7 +56,7 @@ function Projects() {
                 <img src={GroupProject1} className="card-img-top" alt="card-grid-image" />
               </a>
                 <div className="card-body">
-                  <h5 className="card-title">Peak Fitness Group Project #1</h5>
+                  <h5 className="card-title">Peak Fitness UNC Group Project #1</h5>
                   <p className="card-text">In the first Group Project of my class, we built a site that uses the user's entered location to give them
                     the day's weather forecast and a List of the Local Biking Trails. In this Project, I assisted with the styling choices
                     and setting up the API calls.<br></br><br></br>
@@ -70,7 +87,7 @@ function Projects() {
                   <img src={Inked} className="card-img-top" alt="card-grid-image" />
                 </a>
                   <div className="card-body">
-                    <h5 className="card-title">InkedIn Group Project #3</h5>
+                    <h5 className="card-title">InkedIn UNC Group Project #3</h5>
                     <p className="card-text">
                       In This Final project my group used all the skills we had acquired in Full Stack Bootcamp, <br></br>
                       My part in this was the React Routing, and Page Styling.<br></br><br></br>
@@ -101,7 +118,7 @@ function Projects() {
                   <img src={NoteTaker} className="card-img-top" alt="card-grid-image" />
                 </a>
                   <div className="card-body">
-                    <h5 className="card-title">Note Taker App</h5>
+                    <h5 className="card-title">Note Taker Project</h5>
                     <p className="card-text">
                       A Simple note taking app, Allowing a user to write and delete notes, using client side storage.<br></br><br></br>
                       Code Repository <a href="https://github.com/AJoelj84/NoteTaker">Here</a>.
