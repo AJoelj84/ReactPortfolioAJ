b02c54b876e126e099436854c5d46af0af2e475e
AJoelj84 <ajoelj84@gmail.com>
2025-05-19 18:34:31 -0400

    removed unused variable

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,24 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Dropdown from 'react-bootstrap/Dropdown';
 import './assets/styling/fonts.css';
 
 function Navigation() {
-//   return (
-//     <Dropdown class= 'dropdown' style={{ float: 'right', paddingRight: '40px' }}>
-//       <Dropdown.Toggle variant="primary" id="dropdownMenuButton">
-//         Navigation
-//       </Dropdown.Toggle>
-//       <Dropdown.Menu>
-//         <Dropdown.Item as={Link} to="/">About Me</Dropdown.Item>
-//         <Dropdown.Item as={Link} to="/projects">Portfolio</Dropdown.Item>
-//         <Dropdown.Item as={Link} to="/contact">Contact</Dropdown.Item>
-//         <Dropdown.Item as={Link} to="/resume">Resume</Dropdown.Item>
-//       </Dropdown.Menu>
-//     </Dropdown>
-//   );
-// }
-
 
 return (
   <div className='navigation'>
